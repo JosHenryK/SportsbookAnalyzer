927b04d89ae3222d24e9919cbd7a23160ec25f17
JosHenryK <jhkshark@gmail.com>
2025-06-23 10:13:41 -0400

    Restructured player data parsing, fixed typo in README.md

--- CSVParser.py
+++ CSVParser.py
@@ -2,17 +2,25 @@ import os
 import csv
 import json
 
-def parsePlayerData(playerDataDir , parsedPlayerDataDir):
-    for file in os.listdir(playerDataDir):
-        with open(os.path.join(playerDataDir, file), 'r') as readHead:
+def parsePlayerData(DataDir , parsedDataDir):
+    os.makedirs(parsedDataDir , exist_ok = True)
+    for file in os.listdir(DataDir):
+        with open(os.path.join(DataDir, file), 'r') as readHead:
             dict = csv.DictReader(readHead)
+
+            year = file.split("_")[2].split(".")[0]
             for entry in dict:
-                year = file.split("_")[2].split(".")[0]
-                playerName = entry['player_name']
+                week = entry['week']
                 currentTeam = entry['recent_team']
                 opponentTeam = entry['opponent_team']
+                playerName = entry['player_display_name']
 
-                path = os.path.join(parsedPlayerDataDir , year)
+                path = os.path.join(parsedDataDir , year , week , currentTeam + "_v_" + opponentTeam , currentTeam)
+                if year in os.listdir(parsedDataDir):
+                    if week in os.listdir(os.path.join(parsedDataDir, year)):
+                        if (opponentTeam + "_v_" + currentTeam) in os.listdir(os.path.join(parsedDataDir, year, week)):
+                            path = os.path.join(parsedDataDir , year , week , opponentTeam + "_v_" + currentTeam , currentTeam)
+                
                 os.makedirs(path , exist_ok = True)
-                with open(os.path.join(path , playerName + "_" + currentTeam + "_v_" + opponentTeam + ".json") , 'w') as writeHead:
+                with open(os.path.join(path , playerName + ".json") , 'w') as writeHead:
                     writeHead.write(json.dumps(entry))
